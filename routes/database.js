dbf5536e05c72091e2e03c922a3ac28b71979ec9
Knursky <47036541+Knursky@users.noreply.github.com>
2020-04-26 09:58:28 +0200

    ok

---
+++ routes/database.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var firebase = require("firebase-admin");
+var router = express.Router();
+
+var ServiceAccount = require("../database.json");
+
+//Initialization
+firebase.initializeApp({
+  credential: firebase.credential.cert(ServiceAccount),
+  databaseURL: "https://appfire-e3a45.firebaseio.com/",
+});
+
+//seting up connection
+var db = firebase.database();
+var ref = db.ref("students");
+
+ref.once("value", function (snapshot) {
+  console.log(snapshot.val());
+});
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("Database route is working");
+});
+
+module.exports = router;
